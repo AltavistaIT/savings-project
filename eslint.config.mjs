9446f018eb5a3c380564df07eba5ab1d7a97507c
artxga <angelarteaga989@gmail.com>
2025-08-04 23:29:50 -0500

    eslint ignores internalapityhpes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["src/types/internal-api/**/*"]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
